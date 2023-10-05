f550a46ef78e457c1aba3c3e1861d4bef25fd99a
sirguse <115590503+sirguse@users.noreply.github.com>
2023-10-05 19:15:44 +0200

    Add files via upload

---
+++ Laboratorium1/Zadanie1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Zadanie1
+{
+    static void Main()
+    {
+        Console.WriteLine("Sprawdzanie parzystosci liczb");
+        Console.WriteLine();
+        Console.WriteLine("Podaj liczbe");
+        int liczba = int.Parse(Console.ReadLine());
+        if(liczba%2 == 0)
+        {
+            Console.WriteLine("Jest parzysta");
+        }
+        else
+        {
+            Console.WriteLine("Jest nieparzysta");
+        }
+
+
+    }
+}
