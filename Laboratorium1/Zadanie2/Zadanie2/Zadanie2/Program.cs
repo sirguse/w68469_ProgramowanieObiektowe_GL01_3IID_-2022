c8d784a5160961da305a62fa369b75ea07218d2a
sirguse <115590503+sirguse@users.noreply.github.com>
2023-10-05 19:33:17 +0200

    Add files via upload

---
+++ Laboratorium1/Zadanie2/Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość ciągu");
+            int x = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine();
+            for(int i = 0; i < x; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
