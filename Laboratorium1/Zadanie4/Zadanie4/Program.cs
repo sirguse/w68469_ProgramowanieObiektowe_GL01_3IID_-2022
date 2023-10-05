ddd2b006276184f57ab32b53eca8661671bd756b
sirguse <115590503+sirguse@users.noreply.github.com>
2023-10-05 19:38:07 +0200

    Add files via upload

---
+++ Laboratorium1/Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***OBLICZANIE SILNI***");
+            Console.WriteLine();
+            Console.WriteLine("Podaj liczbe");
+
+            int liczba = int.Parse(Console.ReadLine());
+            int silnia = 1;
+            Console.Clear();
+            for (int i =1; i<= liczba; i++)
+            {
+                silnia *= i;
+
+            }
+            Console.WriteLine("Wynik: "+silnia);
+
+        }
+
+    }
+}
