c0006e95023a092e5a9c0f8948a712abfd62956c
sirguse <115590503+sirguse@users.noreply.github.com>
2023-10-05 19:40:14 +0200

    Add files via upload

---
+++ Laboratorium1/Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int x = rand.Next(1, 11);
+            Console.WriteLine("***Znajdz liczbe, masz 3 proby!");
+            for (int i = 0; i < 3; i++)
+            {
+                int z = int.Parse(Console.ReadLine());
+                if (z == x)
+                {
+                    Console.WriteLine("Znaleziono!!!");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Znaleziono!!!");
+                    break;
+                }
+            }
+        }
+    }
+}
